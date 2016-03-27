1966c8cb54fb7766017efba60bb0045f562bf0e8
Frank Giesecke <frank.giesecke@final-gene.de>
2016-03-27 14:28:28 +0200

    Fix ZF2 dependencies

--- src/Module.php
+++ src/Module.php
@@ -8,16 +8,71 @@
 
 namespace FinalGene\MonologModule;
 
+use FinalGene\MonologModule\Monolog\LoggerManager;
+use FinalGene\MonologModule\Service\MonologService;
+use Monolog\ErrorHandler;
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
 use Zend\ModuleManager\Feature\DependencyIndicatorInterface;
+use Zend\Mvc\Application;
 
 /**
  * Module
  *
  * @package FinalGene\MonologModule
  */
-class Module implements ConfigProviderInterface, DependencyIndicatorInterface
+class Module implements BootstrapListenerInterface, ConfigProviderInterface, DependencyIndicatorInterface
 {
+    /**
+     * Listen to the bootstrap event
+     *
+     * @param EventInterface $event
+     *
+     * @return array
+     */
+    public function onBootstrap(EventInterface $event)
+    {
+        $application = $event->getTarget();
+        if (!$application instanceof Application) {
+            return;
+        }
+
+        $serviceManager = $application->getServiceManager();
+        if ($serviceManager->has(MonologService::class) &&
+            $serviceManager->has(LoggerManager::class)
+        ) {
+            /** @var $monologService MonologService */
+            $monologService = $serviceManager->get(MonologService::class);
+
+            /** @var $monologLoggerManager LoggerManager */
+            $monologLoggerManager = $serviceManager->get(LoggerManager::class);
+
+            $this->registerMonologErrorLogger($monologService, $monologLoggerManager);
+        }
+    }
+
+    /**
+     * Register monolog error logger
+     *
+     * @param MonologService $service
+     * @param LoggerManager  $loggerManager
+     */
+    public function registerMonologErrorLogger(MonologService $service, LoggerManager $loggerManager)
+    {
+        $monologConfig = $service->getConfig();
+        if (!isset($monologConfig['error_logger'])) {
+            return;
+        }
+
+        $errorLogger = $monologConfig['error_logger'];
+        if (!$loggerManager->has($errorLogger)) {
+            return;
+        }
+
+        ErrorHandler::register($loggerManager->get($errorLogger));
+    }
+
     /**
      * @inheritdoc
      */
